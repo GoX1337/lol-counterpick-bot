3a7fcb8b9310a8321596dbc42e86b03191739052
gox <gox.roche@gmail.com>
2020-07-08 12:59:33 +0200

    Bot command router

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
-const Discord = require('discord.js');
+const Discord = require('discord.js')
+const lol = require('./lol');
 const client = new Discord.Client();
 
 client.login(process.env.DISCORD_TOKEN);
+const prefix = "!";
+const helpChampionCommand = "!champions <criteria> : Get all champions (optional criteria is champion name substring)";
+const helpCounterCommand = "!counter <champion_name> : Get all counter picks of the given champion"
 
 client.once('ready', () => {
 	console.log(`${client.user.tag} discord bot is ready.`);
@@ -17,8 +21,28 @@ client.on('error', error => {
 	console.error(error);
 });
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('Pong!');
+client.on('message', async message => {
+    if (!message.content.startsWith(prefix) || message.author.bot){
+        return;
     }
+	const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'help') {
+		message.channel.send(`${helpChampionCommand}\n${helpCounterCommand}`);
+	} else if (command === 'champions') {
+        const criteria = args.length > 0 ? args[0] : null;
+        const champions = await lol.getAllChampions(criteria);
+		message.channel.send(champions);
+    } else if (command === 'counter') {
+        const champion = args.length > 0 ? args[0] : null;
+        let msg;
+        if(champion){
+            const counters = await lol.getAllCounters(champion);
+            msg = counters;
+        } else {
+            msg = "!counter: Missing champion name !";
+        }
+		message.channel.send(msg);
+	} 
 });
---
+++ lol.js
@@ -0,0 +1,9 @@
+
+module.exports.getAllChampions = async (criteria) => {
+    return ["Lux", "Jax"];
+}
+
+
+module.exports.getAllCounters = async () => {
+    return ["Annie", "Janna"];
+}
